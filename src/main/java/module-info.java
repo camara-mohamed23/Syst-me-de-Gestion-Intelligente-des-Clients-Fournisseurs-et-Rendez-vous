3475dec8ae1a633f8938c3adb796d753677c80f0
mohamed <camara>
2025-09-03 16:53:38 +0200

    ajoueter

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.gestion {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.gestion to javafx.fxml;
+    exports org.example.gestion;
+}
